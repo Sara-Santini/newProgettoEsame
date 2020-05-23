ef7351b8365b389370ce8ae94f45310b8b73df83
Francesco Voto <S1087880@studenti.univpm.it>
2020-05-23 23:06:11 +0200

    dowImage

--- ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Others/Parsing.java
+++ ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Others/Parsing.java
@@ -3,7 +3,6 @@
  *******************************************************************************/
 package it.bonfire.ProjectOOP.Others;
 
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,7 +10,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Iterator;
-import it.bonfire.ProjectOOP.Others.Downloader;
+
 
 import javax.imageio.ImageIO;
 
@@ -28,80 +27,85 @@ import it.bonfire.ProjectOOP.Model.Image;
  * @author arian
  */
 public class Parsing {
-	 public static int N=0;
+	public static int N = 1;
+
 	/**
 	 * Description of the method GetJSONFromUrl.
 	 */
-	public HashSet<API_Instagram> GetAPIInstagramFromJson(JSONObject jsonObject) 
-	{
-     HashSet<API_Instagram> collezione=new HashSet<>();
-    
-	             JSONArray posizioni = (JSONArray) jsonObject.get("data");
+	public HashSet<API_Instagram> GetAPIInstagramFromJson(JSONObject jsonObject) {
+		HashSet<API_Instagram> collezione = new HashSet<>();
 
-					for (int i = 0; i < posizioni.size(); i++) {
-						jsonObject = (JSONObject) posizioni.get(i);
-						String id = (String) jsonObject.get("id");
-						String caption=(String) jsonObject.get("caption");
-						String media_type = (String) jsonObject.get("media_type");
-						String media_url=(String) jsonObject.get("media_url");
-						
-						if(media_type.equals("CAROUSEL_ALBUM"))
-						{     HashSet<Fotografia> fotografias= new HashSet<Fotografia>();
-						   JSONObject jsonprovo= (JSONObject) jsonObject.get("children");
-							JSONArray album = (JSONArray) jsonprovo.get("data");
-							for (int j = 0; j < album.size(); j++) {
-								
-								JSONObject jsonObject1 = (JSONObject)album.get(j);
-								String albumid = (String) jsonObject1.get("id");
-								
-							  fotografias.add(new Fotografia(albumid));
-								
-								}
-							API_Instagram ciao= (API_Instagram) new Album(id,caption,media_type,fotografias);
-							collezione.add(ciao);
-						}
-						else { API_Instagram appoggio = (API_Instagram) new Image(id,caption,media_type,new Fotografia(id,media_url));
-						collezione.add(appoggio);
-					}}
-					return collezione;
+		JSONArray posizioni = (JSONArray) jsonObject.get("data");
+
+		for (int i = 0; i < posizioni.size(); i++) {
+			jsonObject = (JSONObject) posizioni.get(i);
+			String id = (String) jsonObject.get("id");
+			String caption = (String) jsonObject.get("caption");
+			String media_type = (String) jsonObject.get("media_type");
+			String media_url = (String) jsonObject.get("media_url");
+
+			if (media_type.equals("CAROUSEL_ALBUM")) {
+				HashSet<Fotografia> fotografias = new HashSet<Fotografia>();
+				JSONObject jsonprovo = (JSONObject) jsonObject.get("children");
+				JSONArray album = (JSONArray) jsonprovo.get("data");
+				for (int j = 0; j < album.size(); j++) {
+
+					JSONObject jsonObject1 = (JSONObject) album.get(j);
+					String albumid = (String) jsonObject1.get("id");
+
+					fotografias.add(new Fotografia(albumid));
+
+				}
+				API_Instagram ciao = (API_Instagram) new Album(id, caption, media_type, fotografias);
+				collezione.add(ciao);
+			} else {
+				API_Instagram appoggio = (API_Instagram) new Image(id, caption, media_type,
+						new Fotografia(id, media_url));
+				collezione.add(appoggio);
 			}
-	
-	
-	 public void DownloadImage(HashSet<API_Instagram> collezione) throws IOException {       
-		
-		 try {
-			 Iterator<API_Instagram> l= collezione.iterator();
-			 while(l.hasNext()) {
-				 API_Instagram appoggio=l.next();
-				URL url=new URL(getURL(appoggio.getId()));
-	            BufferedImage image = ImageIO.read(url);
-	        	
-	        if(appoggio.getMedia_type().equals("CAROUSEL_ALBUM")) {
-	       File file = new File("C:\\Users\\arian\\Pictures\\Album"+N);
-	       Album ricerca=(Album) appoggio;
-	      
-	       Iterator<Fotografia> p= ricerca.getFotografias().iterator();
-			while(p.hasNext())
-			{    Fotografia a=p.next();
-				String id=a.getId_photos();
-	   File file1=new File("C:\\Users\\arian\\Pictures\\AlbumN\\" + id  +".jpg");
-	        	  ImageIO.write(image, "jpg", file1);
-	          }
-	            
-	            } }}catch (MalformedURLException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        }
-	       
-	        
-	    }
-	
-	 
-	 public String getURL (String id)
-{ String url= "https://graph.instagram.com/" + id +"?fields=media_url&access_token=IGQVJYcF9aRXNPN1FXVUxnaFptSTZAZAWDdGc19XYk03ajRTOU1PbTJGMFdJb2xmdlR1aV9rVmxfU3BTaUJJd0s5MlNqYlZAxTVV5a1J6cHBXdGpraFhUdDJCR283cmdlYVlGRE55S1g3ZAGRHV3ZATWDNjbwZDZD";
-return url;
+		}
+		return collezione;
+	}
+
+	public void DownloadImage(HashSet<API_Instagram> collezione) throws IOException {
+
+		try {
+			Iterator<API_Instagram> l = collezione.iterator();
+
+			while (l.hasNext()) {
+				API_Instagram appoggio = l.next();
+				if (appoggio.getMedia_type().equals("CAROUSEL_ALBUM")) {
+					Album util = (Album) appoggio;
+
+					Iterator<Fotografia> p = util.getFotografias().iterator();
+					while (p.hasNext()) {
+						Fotografia a = p.next();
+						URL url = new URL(a.getMedia_url());
+						BufferedImage image = ImageIO.read(url);
+						File file = new File(
+								"C:\\Users\\arian\\Pictures\\Album" + N + "\\" + a.getId_photos() + ".jpg");
+						ImageIO.write(image, "jpg", file);
+					}
+					N++;
+				} else {
+					Image util1 = (Image) appoggio;
+					URL url = new URL(util1.getFotografias().getMedia_url());
+					BufferedImage image = ImageIO.read(url);
+					File file = new File("C:\\Users\\arian\\Pictures\\" + util1.getId() + ".jpg");
+					ImageIO.write(image, "jpg", file);
+
+				}
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getURL(String id) {
+		String url = "https://graph.instagram.com/" + id
+				+ "?fields=media_url&access_token=IGQVJYcF9aRXNPN1FXVUxnaFptSTZAZAWDdGc19XYk03ajRTOU1PbTJGMFdJb2xmdlR1aV9rVmxfU3BTaUJJd0s5MlNqYlZAxTVV5a1J6cHBXdGpraFhUdDJCR283cmdlYVlGRE55S1g3ZAGRHV3ZATWDNjbwZDZD";
+		return url;
+	}
 }
-}
-
-
-
