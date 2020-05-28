28f297587414eac87d97f6cbcb40ff4cbd89a64d
Francesco Voto <S1087880@studenti.univpm.it>
2020-05-28 19:46:13 +0200

    cii

--- ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Controller/AppController.java
+++ ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Controller/AppController.java
@@ -3,6 +3,18 @@
  *******************************************************************************/
 package it.bonfire.ProjectOOP.Controller;
 
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.bonfire.ProjectOOP.Model.API_Instagram;
+import it.bonfire.ProjectOOP.Others.Downloader;
+import it.bonfire.ProjectOOP.Others.Parsing;
+
 // Start of user code (user defined imports)
 
 // End of user code
@@ -14,22 +26,23 @@ package it.bonfire.ProjectOOP.Controller;
  * @author Francesco Voto
  * @author Sara Santini
  */
+@RestController
 public class AppController {
-	// Start of user code (user defined attributes for AppController)
+	
+	    HashSet<API_Instagram> dati;
 
-	// End of user code
+			
+			/**
+			 * Risponde all richiesta GET /metadata 
+			 * @return ArrayList di oggetti Metadata
+			 */
 
-	/**
-	 * The constructor.
-	 */
-	public AppController() {
-		// Start of user code constructor for AppController)
-		super();
-		// End of user code
-	}
-
-	// Start of user code (user defined methods for AppController)
-
-	// End of user code
-
+			@RequestMapping(value = "metadata", method=RequestMethod.GET)
+			public HashSet<API_Instagram> getMetadata(){
+				
+				return new DataBase().getApi();
+			}
+			
+			
+			
 }
---
+++ ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Controller/DataBase.java
@@ -0,0 +1,40 @@
+package it.bonfire.ProjectOOP.Controller;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.json.simple.parser.ParseException;
+
+import it.bonfire.ProjectOOP.Model.API_Instagram;
+import it.bonfire.ProjectOOP.Others.Downloader;
+import it.bonfire.ProjectOOP.Others.Parsing;
+
+public class DataBase {
+	private HashSet<API_Instagram> api=new HashSet<API_Instagram>();
+	public HashSet<API_Instagram> getApi() {
+		return api;
+	}
+	public void setApi(HashSet<API_Instagram> api) {
+		this.api = api;
+	}
+	public DataBase() {
+		Downloader iooDownloader=new Downloader();
+		Parsing parsing=new Parsing();
+		String urlString= "https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,children&access_token=IGQVJYcF9aRXNPN1FXVUxnaFptSTZAZAWDdGc19XYk03ajRTOU1PbTJGMFdJb2xmdlR1aV9rVmxfU3BTaUJJd0s5MlNqYlZAxTVV5a1J6cHBXdGpraFhUdDJCR283cmdlYVlGRE55S1g3ZAGRHV3ZATWDNjbwZDZD";
+		try {
+			api=parsing.GetAPIInstagramFromJson(iooDownloader.getJSONbyURL(urlString));
+			iooDownloader.getImageAlbumUrl(api);
+			parsing.DownloadImage(api);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+
+}
