8841add503f60638637de00e5be7dae76f13fb8a
Arianna Nazzarelli <s1087270@studenti.univpm.it>
2020-06-23 18:53:36 +0200

    changes in FilterService

--- ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Filters/FilterService.java
+++ ProjectOOP/src/main/java/it/bonfire/ProjectOOP/Filters/FilterService.java
@@ -18,76 +18,7 @@ import it.bonfire.ProjectOOP.Model.API_Instagram;
  * @author Francesco Voto
  * @author Sara Santini
  */
-//
-//public class FilterService {
-//	/**
-//	 * New collection of API_instagram.
-//	 */
-//	private HashSet<API_Instagram> api = new HashSet<API_Instagram>();
-//	/**
-//	 * New object of filter.
-//	 */
-//	public Filter filter = new Filter();
-//
-//	/**
-//	 * The constructor.
-//	 * 
-//	 * @param util A collection of API_Instagram.
-//	 */
-//	public FilterService(HashSet<API_Instagram> util) {
-//		api = util;
-//	}
-//
-//	/**
-//	 * Method that gives the collection.
-//	 * @see API_Instagram api
-//	 * @return HashSet
-//	 */
-//	public HashSet<API_Instagram> getApi() {
-//		return api;
-//	}
-//
-//	/**
-//	 * Method which sets a value to attribute to the collection.
-//	 * 
-//	 * @param api Collection of API_Instagram.
-//	 */
-//	public void setApi(HashSet<API_Instagram> api) {
-//		this.api = api;
-//	}
-//
-//	/**
-//	 * Method that manages the requests of filter with jolly character
-//	 * 
-//	 * @param field The jolly character that gives the requested filter by the user.
-//	 * @param a     The value the user wants.
-//	 * @see Filter filter
-//	 * @throws FilterNotFoundException It is thrown when the filter doesn't exists. 
-//	 */
-//
-//	public void Operator(String field, int a) throws FilterNotFoundException{
-//		
-//		if (field.equals("#")) {
-//			api = filter.photosWithHashtag(api);
-//		}
-//		
-//		else if (field.equals("|")) {
-//			api = filter.PhotosHeight(api, a);
-//
-//		}
-//		else if (field.equals("-")) {
-//			api = filter.PhotosWide(api, a);
-//		}
-//		else if (field.equals("?")) {
-//			api = filter.photosMore100Kb(api, a);
-//		}
-//		else if (field.equals("!")) {
-//			api = filter.photosUnder100Kb(api, a);
-//		}
-//		else throw new FilterNotFoundException();
-//		
-// 
-//}
+
 public class FilterService {
 	/**
 	 * New collection of API_instagram.
@@ -187,5 +118,14 @@ public class FilterService {
 			throw new EmptyCollectionException();
 		;
 		return api;
+	}
+	
+	/**
+	 * Method which sets a value to attribute to the collection.
+	 * 
+	 * @param api Collection of API_Instagram.
+	 */
+	public void setApi(HashSet<API_Instagram> api) {
+		this.api = api;
 	}
 }
